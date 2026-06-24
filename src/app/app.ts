445bf5d70a678f5aac8b037a2691376711c226a8
David Groß <d.g1r2o3s4s@gmail.com>
2026-06-24 15:47:13 +0200

    imported landing-page-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
+import { LandingPage } from "./shared/components/landing-page/landing-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
