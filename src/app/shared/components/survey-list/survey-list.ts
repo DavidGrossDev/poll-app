9a82a9584354e126fa02e317bbc233d994e1deba
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-08 07:47:58 +0200

    edit surey-list component

--- src/app/shared/components/survey-list/survey-list.ts
+++ src/app/shared/components/survey-list/survey-list.ts
@@ -6,4 +6,37 @@ import { Component } from '@angular/core';
   templateUrl: './survey-list.html',
   styleUrl: './survey-list.scss',
 })
-export class SurveyList {}
+export class SurveyList {
+  list = [
+    {
+      category: "Team activities",
+      content : "Let’s Plan the Next Team Event Together",
+      ending: "Ends in 1 Day"
+    },
+    {
+      category: "activities",
+      content : "Let’s Plan",
+      ending: "Ends in 6 Day"
+    },
+    {
+      category: "Team ",
+      content : "Team Event Together",
+      ending: "Ends in 8 Day"
+    },
+    {
+      category: "activ",
+      content : "Plan Together",
+      ending: "Ends in 16 Day"
+    },
+    {
+      category: "ties",
+      content : "Next Team",
+      ending: "Ends in 7 Day"
+    },
+    {
+      category: "eamties",
+      content : "Plan Next Team r",
+      ending: "Ends in 3 Day"
+    }
+  ]
+}
