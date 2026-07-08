1cfe6f7f81ffd9bc5efac5cb7368253cd7548eda
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-08 07:49:03 +0200

    edit landing-page component

--- src/app/shared/components/landing-page/landing-page.ts
+++ src/app/shared/components/landing-page/landing-page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LandingSection } from '../landing-section/landing-section';
 import { EndingSoonList } from '../ending-soon-list/ending-soon-list';
 import { SurveyList } from '../survey-list/survey-list';
 
 
+
 @Component({
   selector: 'app-landing-page',
-  imports: [EndingSoonList, SurveyList],
+  imports: [ LandingSection, SurveyList, EndingSoonList],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
-export class LandingPage {}
+export class LandingPage {
+  ngOnInit() {
+    document.body.classList.remove('default', 'bckgrdclr-wh');
+    document.body.classList.add('default');
+  }
+  
+}
