d78f657791ee4c3428a299f7419f46f834205c4f
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-08 07:47:11 +0200

    added endingSoonList[ ]

--- src/app/shared/components/ending-soon-list/ending-soon-list.ts
+++ src/app/shared/components/ending-soon-list/ending-soon-list.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './ending-soon-list.html',
   styleUrl: './ending-soon-list.scss',
 })
-export class EndingSoonList {}
+export class EndingSoonList {
+  endingSoonList = [
+    {
+      category: "Team activities",
+      content : "Let’s Plan the Next Team Event Together",
+      ending: "Ends in 1 Day"
+    },
+    {
+      category: "Health & Wellness",
+      content : "Fit & wellness survey!",
+      ending: "Ends in 2 Days"
+    },
+    {
+      category: "Gaming & Entertainment",
+      content : "Gaming habits and favorite games!",
+      ending: "Ends in 3 Days"
+    }
+  ]
+}
