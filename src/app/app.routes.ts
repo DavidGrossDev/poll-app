5ab894fd0a52a3fc9b35cad779faee14324e6956
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-08 07:48:41 +0200

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from "../app/shared/components/landing-page/landing-page";
+import { SurveyView } from "../app/shared/components/survey-view/survey-view";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LandingPage
+    },
+    {
+        path: "viewSurvey",
+        component: SurveyView
+    }
+];
