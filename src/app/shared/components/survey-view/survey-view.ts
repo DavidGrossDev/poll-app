1d78a17af638e5557b1fdee6c61da91bb71a3ff4
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-12 19:01:57 +0200

    added resultsA[] for testing

--- src/app/shared/components/survey-view/survey-view.ts
+++ src/app/shared/components/survey-view/survey-view.ts
@@ -11,4 +11,23 @@ export class SurveyView {
     document.body.classList.remove('default', 'bckgrdclr-wh');
     document.body.classList.add('bckclr-wh');
   }
+
+  resultsA = [
+    {
+      answer:"A",
+      value : 27
+    },
+    {
+      answer:"B",
+      value : 44
+    },
+    {
+      answer:"C",
+      value : 3
+    },
+    {
+      answer:"D",
+      value : 26
+    }
+  ];
 }
