1581d34443c48094a911b42c30aaa193cfd3d799
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-14 05:27:44 +0200

    edit variable

--- src/app/shared/components/survey-view/survey-view.ts
+++ src/app/shared/components/survey-view/survey-view.ts
@@ -12,22 +12,93 @@ export class SurveyView {
     document.body.classList.add('bckclr-wh');
   }
 
-  resultsA = [
+  surveyResults = [
     {
-      answer:"A",
-      value : 27
+      "question": "Which date would work best for you?",
+      "answers" :[
+        {
+          answer: "A",
+          value: 27
+        },
+        {
+          answer: "B",
+          value: 44
+        },
+        {
+          answer: "C",
+          value: 3
+        },
+        {
+          answer: "D",
+          value: 26
+        }
+      ]
+      
     },
     {
-      answer:"B",
-      value : 44
+      "question": "Choose the activities you prefer?",
+     "answers": [
+        {
+          answer: "A",
+          value: 60
+        },
+        {
+          answer: "B",
+          value: 0
+        },
+        {
+          answer: "C",
+          value: 14
+        },
+        {
+          answer: "D",
+          value: 26
+        },
+        {
+          answer: "E",
+          value: 0
+        }
+      ]
     },
     {
-      answer:"C",
-      value : 3
+      "question": "What’s most important to you in a team event?",
+      "answers": [
+        {
+          answer: "A",
+          value: 44
+        },
+        {
+          answer: "B",
+          value: 3
+        },
+        {
+          answer: "C",
+          value: 26
+        },
+        {
+          answer: "D",
+          value: 27
+        }
+      ]
     },
     {
-      answer:"D",
-      value : 26
+      "question": "How long would you prefer the event to last?",
+      "answers": [
+        {
+          answer: "A",
+          value: 14
+        },
+        {
+          answer: "B",
+          value: 86
+        },
+        {
+          answer: "C",
+          value: 0
+        }
+      ]
     }
   ];
+
+
 }
