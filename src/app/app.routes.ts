b04df22c78a506baa39901d714be0b9cc34bcfce
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-14 13:26:23 +0200

    added an new route( createSurvey)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from "../app/shared/components/landing-page/landing-page";
 import { SurveyView } from "../app/shared/components/survey-view/survey-view";
+import {CreateSurvey} from "../app/shared/components/create-survey/create-survey";
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "viewSurvey",
         component: SurveyView
+    },
+    {
+        path: "createSurvey",
+        component: CreateSurvey
     }
 ];
