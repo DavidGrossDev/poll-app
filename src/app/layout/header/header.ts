e89b7b5fa68b1eb2abc31bf8730d718d25ff6f96
David Groß <d.g1r2o3s4s@gmail.com>
2026-07-14 13:27:01 +0200

    added btn and a variable in ts for tests

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  ngOnInit() {
+    
+  }
+  path : string = "";
+}
