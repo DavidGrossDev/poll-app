12044d8e88014a1617607b3619b0c54c40f601b7
David Groß <d.g1r2o3s4s@gmail.com>
2026-08-17 19:42:47 +0200

    added routerlink (createSurvey)

--- src/app/shared/components/landing-section/landing-section.ts
+++ src/app/shared/components/landing-section/landing-section.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-landing-section',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-section.html',
   styleUrl: './landing-section.scss',
 })
